99af510f27a85ec513572c447cab073819aeda25
Allison Kim <akeem159@yahoo.com>
2022-08-01 15:08:07 -0400

    Add filter dropdown

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menu = document.querySelector('#cocktail-menu')
 
-    function fetchCocktails(url) {
-        fetch(url)
+    // Functions
+    function renderFilter() {
+        fetchData('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+        .then(data => createFilterOptions(data))
+    }
+
+    function createFilterOptions(dataObj) {
+        const filterMenu = document.querySelector('#select-ingredient')
+        dataObj.drinks.forEach(ingredient => {
+            const inOption = document.createElement('option')
+            inOption.value = ingredient.strIngredient1
+            inOption.textContent = ingredient.strIngredient1
+            filterMenu.append(inOption)
+        });
+    }
+
+    function fetchData(url) {
+        return fetch(url)
         .then(res => res.json())
-        .then(data => displayCocktails(data))
     }
 
     function displayCocktails(dataObj) {
+        const menu = document.querySelector('#cocktail-menu')
         dataObj.drinks.forEach(cocktail => {
             const div = document.createElement('div')
             const img = document.createElement('img')
@@ -19,9 +34,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
-    fetchCocktails('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka')
+    renderFilter()
+    
+    // fetchData('https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=tequila')
 
-    // fetch(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=vodka`)
-    // .then(res => res.json())
-    // .then(data => console.log(data.drinks))
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list`)
+    .then(res => res.json())
+    .then(data => console.log(data.drinks))
 })
