1581949cf05e9f4a87615980fc88841b503a3407
Allison Kim <akeem159@yahoo.com>
2022-08-02 11:35:40 -0400

    Add Sheena css styling

--- index.js
+++ index.js
@@ -1,52 +1,32 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Global Variables
-    const rating = document.querySelector('#rating')
-    const comment = document.querySelector('#comment')
-    const form = document.querySelector('#edit-cocktail')
+    const updateForm = document.querySelector('#edit-cocktail')
     let curCocktail = {}
 
-    // Fetch Request
+// Basic Reusable Fetch Request
     function fetchData(url) {
         return fetch(url)
         .then(res => res.json())
     }
 
-    // Rendering Functions
-    function renderFilter() {
-        fetchData('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
-        .then(data => renderFilterOptions(data))
-    }
-
-    function renderFilterOptions(dataObj) {
-        const filterMenu = document.querySelector('#select-ingredient')
-        dataObj.drinks.forEach(ingredient => {
-            const ingOption = document.createElement('option')
-            ingOption.value = ingredient.strIngredient1
-            ingOption.textContent = ingredient.strIngredient1
-            filterMenu.append(ingOption)
-        });
-        filterMenu.addEventListener('change', (e) => {
-            let alcohol = e.target.value.replace(/\s/g,"_")
-            renderCocktailMenu(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`)
-        })
-    }
-
+// Rendering Functions
+    // This function populates the cocktail menu with a list of cocktails given by a fetch request
     function renderCocktailMenu(url) {
         fetchData(url)
         .then(data => {
             renderEachCocktail(data)
             renderOneCocktail(data.drinks[0])
-            form.addEventListener('submit', handleForm)
+            updateForm.addEventListener('submit', handleForm)
         })
     }
 
+    // This function creates an image and title element for each cocktail and appends to the menu
     function renderEachCocktail(dataObj) {
         const menu = document.querySelector('#cocktail-menu')
 
         document.querySelectorAll('div.displayed').forEach(e => e.remove())
 
         dataObj.drinks.forEach(cocktail => {
-
             const div = document.createElement('div')
             const img = document.createElement('img')
             const name = document.createElement('h4')
@@ -60,27 +40,61 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     }
 
+    // This function populates the detailed section with the selected cocktail
     function renderOneCocktail(cocktail) {
-        const name = document.querySelector('#name')
-        const recipe = document.querySelector('#recipe')
+        const detailRating = document.querySelector('#rating')
+        const detailComment = document.querySelector('#comment')
+        const detailName = document.querySelector('#name')
+        const detailRecipe = document.querySelector('#recipe')
         const detailImage = document.querySelector('#detail-image')
 
-        fetchData(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${cocktail.idDrink}`)
+        curCocktail = cocktail
+        detailRating.textContent = curCocktail.rating
+        detailComment.textContent = curCocktail.comment
+        
+        fetchData(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${curCocktail.idDrink}`)
         .then(data => {
             detailImage.src = data.drinks[0].strDrinkThumb
-            name.textContent = data.drinks[0].strDrink
-            recipe.textContent = data.drinks[0].strInstructions
+            detailName.textContent = data.drinks[0].strDrink
+            detailRecipe.textContent = data.drinks[0].strInstructions
         })
     }
 
+    // This function updates the rating and comment when the update form is submitted
     function handleForm(e) {
         e.preventDefault()
-        rating.textContent = e.target['update-rating'].value
-        comment.textContent = e.target['update-comment'].value
-        form.reset()
+        curCocktail['rating'] = e.target['update-rating'].value
+        curCocktail['comment'] = e.target['update-comment'].value
+        renderOneCocktail(curCocktail)
+        updateForm.reset()
     }
+
+    // This function generates the dropdown selection of ingredients to filter by
+    function renderFilter() {
+        fetchData('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list')
+        .then(data => renderFilterOptions(data))
+    }
+
+    // This function creates an option element for each ingredient and appends to the selection
+    function renderFilterOptions(dataObj) {
+        const filterMenu = document.querySelector('#select-ingredient')
+
+        dataObj.drinks.forEach(ingredient => {
+            const ingOption = document.createElement('option')
+            ingOption.value = ingredient.strIngredient1
+            ingOption.textContent = ingredient.strIngredient1
+            filterMenu.append(ingOption)
+        });
+
+        filterMenu.addEventListener('change', (e) => {
+            let alcohol = e.target.value.replace(/\s/g,"_")
+            renderCocktailMenu(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${alcohol}`)
+        })
+    }
+
 
     // Invoking Functions
     renderFilter()
     renderCocktailMenu(`https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic`)
+
 })
